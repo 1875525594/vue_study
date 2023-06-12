d80a478daba8598452c5b6355ad2c434f8ca9224
guo <1875525594@qq.com>
2023-06-12 22:14:52 +0800

    first commit

---
+++ 19_单文件组件/App.vue
@@ -0,0 +1,22 @@
+<template>
+   <div>
+     <School></School>
+     <Student></Student>
+   </div>
+</template>
+
+<script>
+//引入组件
+import School from './School';
+import Student from "./Student";
+
+export default {
+  name: "App",
+  //汇总所有的组件
+  components:{
+    Student,
+    School
+  }
+}
+</script>
+
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# vue_study 基础
-vue笔记
-分3部分，基础到vue2，到vue3
+# vue-learning
+学习vue的基础知识
